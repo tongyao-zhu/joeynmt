38b4177a2746572e54a2edeb47409293d93842fc
Tongyao <312404930zhu@gmail.com>
2020-07-12 21:27:49 +0800

    modify script to add token to feature tensor

--- joeynmt/add_token_to_features_tensor.py
+++ joeynmt/add_token_to_features_tensor.py
@@ -5,34 +5,25 @@
 
 
 import torch
-
-
+import argparse
 # In[11]:
 
+def add_tokens(tensor_path, save_path):
 
-features_tensor = torch.load("../f_features_tensor.pt")
+    features_tensor = torch.load("../f_features_tensor.pt")
+    print(f"input features_tensor shape {features_tensor.shape}")
+    print(f"the max value in feature_tensor is {features_tensor.max()}")
+    print(f"the min value in feature tensor is {features_tensor.min()}")
+    special_tokens_tensor = torch.zeros([4, features_tensor.shape[1]])
 
+    special_tokens_tensor[0, :] = -20
+    special_tokens_tensor[2, :] = -10
+    special_tokens_tensor[3, :] = 10
+    features_tensor = torch.cat([special_tokens_tensor, features_tensor])
 
-# In[12]:
-
-
-print(f"input features_tensor shape {features_tensor.shape}")
-
-
-# In[13]:
-
-
-features_tensor.max()
-
-
-# In[14]:
-
-
-features_tensor.min()
-
-
-# In[ ]:
-
+    print(f"features tensor now has shape {features_tensor.shape}")
+    torch.save(features_tensor, save_path)
+    print("finished saving to {}".format(save_path))
 
 # transposed_features_tensor = torch.transpose(features_tensor, 0,1)
 # transposed_features_tensor = torch.cat([transposed_features_tensor, torch.zeros([3, transposed_features_tensor.shape[1]])])
@@ -48,30 +39,13 @@ features_tensor.min()
 # features_tensor = torch.cat([special_tokens_tensor, features_tensor])
 
 
-# In[16]:
 
 
-special_tokens_tensor = torch.zeros([4,features_tensor.shape[1]])
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
 
-special_tokens_tensor[0,:]=-20
-special_tokens_tensor[2,:]=-10
-special_tokens_tensor[3,:]=10
-
-
-# In[17]:
-
-
-features_tensor = torch.cat([special_tokens_tensor, features_tensor])
-
-
-# In[18]:
-
-
-print(f"features tensor now has shape {features_tensor.shape}")
-
-
-# In[ ]:
-
-
-torch.save(features_tensor, "./complete_features_tensor_1024_dimension.pt")
-
+    ap.add_argument("--tensor_path", type = str, required=True)
+    ap.add_argument("--save_path", type = str, required=True)
+    args = ap.parse_args()
+    print(f"you've entered the following arguments: {args}")
+    add_tokens(args.tensor_path, args.save_path)
